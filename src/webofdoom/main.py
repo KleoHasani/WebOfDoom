df3a0f31778806a6a76bdd300f214af282969d34
Kleo <>
2022-03-15 09:18:29 -0400

    added logo and basic CLI.

--- src/webofdoom/main.py
+++ src/webofdoom/main.py
@@ -1,3 +1,33 @@
+import click
 
-def cli():
+@click.group(invoke_without_command=True)
+@click.option("-t", default="http://localhost:3000/", help="Target url or IP address Ex: (http://localhost:3000/)")
+@click.option("-w", type=click.Path(exists=True), help="Wordlist to use for endpoint busting.")
+def cli(t: str, w: str):
+    # Print logo.
+    click.echo(click.style("""
+         _______
+        |.-----.|
+        ||x . x||
+        ||_.-._||
+        `--)-(--`
+       __[=== o]___
+      |:::::::::::|\\
+      `-=========-`()
+""", fg="red"))
+
+    click.echo(click.style(f'{" " * 8 + "WebOfDoom" + " " * 4}', fg="green"))
+    click.echo(click.style("Made with <3 by Kleo Hasani", fg="blue"))
+
+    print("\n")
+
+    # Print target and attack info.
+    click.echo(f'{click.style("  Attack", fg="red")} -> {click.style(t, fg="yellow")}')
+    click.echo(f'{click.style("Wordlist", fg="red")} -> {click.style(w, fg="yellow")}')
+    click.echo(f'{click.style("  Method", fg="red")} -> {click.style("GET", fg="yellow")}')
+
+    print("\n")
+
+    # Send requests.
+
     pass
